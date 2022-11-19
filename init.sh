2ff967915cfce0469a6870b7842306fdd7d612f8
Donghyeon Lee <earthicko@gmail.com>
2022-11-19 17:06:44 +0900

    add lubuntu-rc.xml

--- init.sh
+++ init.sh
@@ -1,22 +1,26 @@
 VIMRC_SRC="./src/vimrc"
 VIMRC_DST="$HOME/.vimrc"
+LUBUNTURC_SRC="./src/lubuntu-rc.xml"
+LUBUNTURC_DST="$HOME/.config/openbox/lubuntu-rc.xml"
 
-write_vimrc () {
-	cp $VIMRC_SRC $VIMRC_DST
+write_with_confirm () {
+	if test -f "$1"
+	then
+		read -p "$1 already exists. Replace? " INPUT
+		if [ "$INPUT" = "Y" ] || [ "$INPUT" = "y" ]
+		then
+			cp $1 $2
+		fi
+	else
+		cp $1 $2
+	fi
 }
 
 echo "Setting .vimrc in $VIMRC_DST"
-if test -f "$VIMRC_DST"
-then
-	read -p "$VIMRC_DST already exists. Replace? " INPUT
-	if [ "$INPUT" = "Y" ] || [ "$INPUT" = "y" ]
-	then
-		write_vimrc
-	fi
-else
-	write_vimrc
-fi
+write_with_confirm $VIMRC_DST $VIMRC_SRC
 
 echo "Setting git configs"
 bash ./src/setup-git.sh
 
+echo "Setting openbox config in $LUBUNTURC_DST"
+write_with_confirm $LUBUNTURC_DST $LUBUNTURC_SRC
